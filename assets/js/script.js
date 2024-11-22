5fa71c3f8c29b9621dc768f14da5ece1dcc2974c
Chien Chou <jeffchen1118@gmail.com>
2024-11-22 18:21:55 +0000

    Fix displaySubstractQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,10 +32,6 @@ function runGame(gameType) {
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "substract") {
-        while ((num1 - num2) < 0) {
-            num1 = Math.floor(Math.random() * 25) +1;
-            num2 = Math.floor(Math.random() * 25) +1;
-        }
         displaySubstractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
@@ -118,8 +114,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubstractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 > operand1 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 
 }
