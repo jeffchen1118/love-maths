8804acae628c18814ea73eed50ee4e1630d08d6d
Chien Chou <jeffchen1118@gmail.com>
2024-11-22 12:17:31 +0000

    setup structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log('Hello, world!');
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}   
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
